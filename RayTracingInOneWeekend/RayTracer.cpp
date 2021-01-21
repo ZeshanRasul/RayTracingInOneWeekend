a2461ee60503e24aadecde8f1ebe42077941ca84
Zeshan Rasul <zeshanrasul@gmail.com>
2021-01-21 00:40:03 +0000

    Shoot rays from fixed camera, output image to ppm

--- RayTracingInOneWeekend/RayTracer.cpp
+++ RayTracingInOneWeekend/RayTracer.cpp
@@ -1,41 +1,56 @@
-#include "FreeImage.h"
 #include "string"
 #include <iostream>
 #include "vec3.h"
 #include "colour.h"
+#include "ray.h"
+
+colour ray_colour(const Ray& r)
+{
+	vec3 unit_direction = unit_vector(r.getDirection());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-	std::string outputFilename = "Raytracer.png";
-	int width = 256;
-	int height = 256;
-	unsigned char* pixels = new unsigned char[size_t(width) * height * 3];
 
+	// Image
 
-	for (int j = height - 1; j >= 0; j--)
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	
+	// Camera
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	int focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = vec3(origin - horizontal / 2.0 - vertical / 2.0 - vec3(0, 0, focal_length));
+
+	// Render
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; j--)
 	{
-		std::cout << "Scanlines remaining: " << j << "\nPercent remaining: " << (float(j) / height) * 100 << "%\n";
-		for (int i = 0; i < width - 1; i++)
+		std::cerr << "Scanlines remaining: " << j << "\nPercent remaining: " << (double(j) / image_height) * 100 << "%\n" << std::flush;
+		for (int i = 0; i < image_width - 1; i++)
 		{
 
-			unsigned char col[3];
+			auto u = double(i) / (image_width - 1.0);
+			auto v = double(j) / (image_height - 1.0);
+			Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			colour final_colour = ray_colour(r);
 
-			float r = float(i) / (width - 1);
-			float g = float(j) / (height - 1);
-			float b = 0.25;
+			write_colour(std::cout, final_colour);
 
-			colour final_colour(r, g, b);
-			
-			write_colour(col, final_colour);
-
-			memcpy(&pixels[((j * width) + i) * 3], &col, 3);
 		}
 	}
 
-	std::cout << "\nDone.\n";
+	std::cerr << "\nDone.\n";
 
-	FreeImage_Initialise();
-	FIBITMAP* img = FreeImage_ConvertFromRawBits((BYTE*)pixels, width, height, 3 * width, 24, 0xFF0000, 0x00FF00, 0x0000FF, false);
-	FreeImage_Save(FIF_PNG, img, outputFilename.c_str(), 0);
-	FreeImage_DeInitialise();
 }
--- RayTracingInOneWeekend/colour.h
+++ RayTracingInOneWeekend/colour.h
@@ -3,13 +3,11 @@
 #include "vec3.h"
 #include <minmax.h>
 
-unsigned char* write_colour(unsigned char* col, colour pixel_colour)
+void write_colour(std::ostream &out, colour pixel_colour)
 {
-	col[0] = unsigned char(min(255.99 * pixel_colour.z(), 255.0f));
-	col[1] = unsigned char(min(255.99 * pixel_colour.y(), 255.0f));
-	col[2] = unsigned char(min(255.99 * pixel_colour.x(), 255.0f));
-
-	return col;
+	out << static_cast<int>(255.99 * pixel_colour.x()) << ' '
+		<< static_cast<int>(255.99 * pixel_colour.y()) << ' '
+		<< static_cast<int>(255.99 * pixel_colour.z()) << '\n';
 }
 
 
