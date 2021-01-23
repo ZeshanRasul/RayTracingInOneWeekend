9cafa27fac9443cb6c2da8d7b7e9174ae5b7f84b
Zeshan Rasul <zeshanrasul@gmail.com>
2021-01-23 13:29:07 +0000

    Positionable Camera

--- RayTracingInOneWeekend/RayTracer.cpp
+++ RayTracingInOneWeekend/RayTracer.cpp
@@ -7,7 +7,7 @@
 #include "material.h"
 
 #include <iostream>
-/*
+
 colour ray_colour(const ray& r, const hittable& world, int depth)
 {
 	hit_record rec;
@@ -31,29 +31,6 @@ colour ray_colour(const ray& r, const hittable& world, int depth)
 	auto t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
 }
-*/
-
-colour ray_colour(const ray& r, const hittable& world, int depth) {
-	hit_record rec;
-
-	// If we've exceeded the ray bounce limit, no more light is gathered.
-	if (depth <= 0)
-		return colour(0, 0, 0);
-
-	if (world.hit(r, 0.001, infinity, rec)) {
-		ray scattered;
-		colour attenuation;
-		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-		{
-			return attenuation * ray_colour(scattered, world, depth - 1);
-		}
-		return colour(0, 0, 0);
-	}
-
-	vec3 unit_direction = unit_vector(r.getDirection());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
-}
 
 
 int main()
@@ -67,22 +44,23 @@ int main()
 	const int max_depth = 10;
 
 	// World
+
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
-	auto material_center = make_shared<dielectric>(1.5);
-	auto material_left = make_shared<metal>(colour(0.8, 0.8, 0.8), 0.0);
-	auto material_right = make_shared<dielectric>(1.5);
+	auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
+	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.0);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1), -0.45, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-//	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), -0.4, material_right));
 
 	// Camera
 
-	camera cam;
+	camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90.0,  aspect_ratio);
 
 	// Render
 
