4adf056ae02e2a86e97389dca5189a7740fda7d5
Zeshan Rasul <zeshanrasul@gmail.com>
2021-01-24 01:29:52 +0000

    Scene Render

--- RayTracingInOneWeekend/RayTracer.cpp
+++ RayTracingInOneWeekend/RayTracer.cpp
@@ -92,11 +92,11 @@ int main()
 {
 
 	// Image
-	const auto aspect_ratio = 3.0 / 9.0;
-	const int image_width = 900;
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width  / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_depth = 50;
+	const int samples_per_pixel = 20;
+	const int max_depth = 10;
 
 	// World
 
